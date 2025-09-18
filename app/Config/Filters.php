27e77d0871462bdcc1fc8778028271ef88e2488a
ALVIAN SYAH BURHANI <105841103522@student.unismuh.ac.id>
2025-09-18 14:50:18 +0800

    dashboard/tampilan

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -34,6 +34,7 @@ class Filters extends BaseFilters
         'forcehttps'    => ForceHTTPS::class,
         'pagecache'     => PageCache::class,
         'performance'   => PerformanceMetrics::class,
+        'auth'          => \App\Filters\AuthFilter::class, // Tambahkan baris ini
     ];
 
     /**
@@ -107,4 +108,6 @@ class Filters extends BaseFilters
      * @var array<string, array<string, list<string>>>
      */
     public array $filters = [];
+
+    
 }
--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,3 +10,15 @@ $routes->get('/register', 'AuthController::register');
 $routes->post('/register', 'AuthController::processRegister');
 $routes->get('/login', 'AuthController::login');
 $routes->post('/login', 'AuthController::processLogin');
+
+$routes->get('/dashboard', 'Dashboard::index');
+
+
+
+// Routes untuk halaman yang memerlukan login
+$routes->group('', ['filter' => 'auth'], static function ($routes) {
+    $routes->get('/dashboard', 'Dashboard::index');
+});
+
+// Tambahkan juga route untuk logout
+$routes->get('/logout', 'AuthController::logout');
